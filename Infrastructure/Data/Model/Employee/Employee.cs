00153ade284cd334efef66f5ee830fa301b6437a
invailid-expression <rohitsurwade34@gmail.com>
2024-09-26 21:02:39 +0530

    add web api project

---
+++ Infrastructure/Data/Model/Employee/Employee.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Data.Model.Employee
+{
+    public class Employee
+    {
+        [Key]
+        public int ID { get; set; }
+
+
+        public string? Name { get; set; }
+
+
+        public string? Email { get; set; }
+
+
+        public string? Contact { get; set; }
+
+
+        public string? Address { get; set; }
+
+
+        //public IFormFile? ProfileImage { get; set; }
+        public string? ProfilePicture { get; set; }
+        public bool? IsActive { get; set; }
+        public DateTime? CreateDate { get; set; }
+    }
+}
---
+++ Infrastructure/Repositories/IEmployee.cs
@@ -0,0 +1,19 @@
+﻿using ApplicationCore.EmployeeModel;
+using Infrastructure.Data.Model.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Repositories
+{
+    public interface IEmployeeService
+    {
+        Task<IEnumerable<Employee>> GetCustomers();
+        Task<ServiceResult<Employee>> GetCustomer(int id);
+        Task<Response> PostCustomers(Employee customer);
+        Task<Response> PutCustomers(int id, Employee customers);
+        Task<Response> DeleteCustomers(int id);
+    }
+}
